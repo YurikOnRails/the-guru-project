744abd766c0685259bd2d2c1a040730ff02ac862
yyyrk <andrei.iurik@gmail.com>
2025-03-05 00:41:35 +0300

    feat: add views, helpers, and footer with project info, implement question count for tests

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(question)
+    action = question.new_record? ? "Create New Question:" : "Edit Question:"
+    "#{action} #{question.test.title}"
+  end
 end
