d87ae5c5169cc6c7438b516f1809b39a0246b2c4
yyyrk <andrei.iurik@gmail.com>
2025-04-03 03:39:28 +0300

    feat: add form name for signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_02_080921) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_003617) do
   create_table "answers", force: :cascade do |t|
     t.string "content", null: false
     t.boolean "correct", default: false, null: false
@@ -67,7 +67,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_02_080921) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
