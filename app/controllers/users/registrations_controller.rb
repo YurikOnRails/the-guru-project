ff827ed823ec49b4e48da50840075f7a4832a051
yyyrk <andrei.iurik@gmail.com>
2025-04-05 17:27:00 +0300

    fix: errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,3 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # Пустой контроллер для возможного расширения в будущем
-end 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise  :database_authenticatable, :registerable, :recoverable,
-          :rememberable, :trackable, :validatable, :confirmable
+          :rememberable, :validatable, :confirmable
 
   has_many :test_passages, dependent: :destroy # чтобы при удалении юзера удалялись и результаты
   has_many :tests, through: :test_passages, dependent: :destroy # соответв. будут удаляться и тесты при удалении юзера
