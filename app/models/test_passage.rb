fa508ff284bc208c3d7d00ff0fa861e48e488583
yyyrk <andrei.iurik@gmail.com>
2025-04-07 13:57:44 +0300

    feat: add Devise authentication and UI improvements

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,6 +1,7 @@
 class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
+  belongs_to :user, optional: true
 
   validates :correct_questions, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :test_has_questions, on: :create
