4a27adedfdbb49f16395651eaca338b54e581082
yyyrk <andrei.iurik@gmail.com>
2025-04-08 18:55:30 +0300

    feat: added localization for Russian and English

--- Gemfile
+++ Gemfile
@@ -49,7 +49,7 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
-#LOCALISATION
+# LOCALISATION
 gem "rails-i18n"
 
 group :development, :test do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
-    { lang: I18n.locale }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale } # для выполнения 3 пункта задания
   end
 
   protected
