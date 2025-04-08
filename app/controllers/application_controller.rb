10a8f13beec2ffdac3cf8756d3166449d80d4ec5
yyyrk <andrei.iurik@gmail.com>
2025-04-08 16:43:30 +0300

    feat: add a little bit localize and fix 1 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   protected
 
@@ -18,5 +23,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name ])
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
