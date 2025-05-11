7da4a6be9f5cd544e801d4d3c029c09044ac5948
yyyrk <andrei.iurik@gmail.com>
2025-05-11 15:59:04 +0300

    feat: add confirmation after registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout, sign_up: :signup }, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout, sign_up: :signup }, controllers: { sessions: "users/sessions", registrations: "users/registrations", confirmations: "users/confirmations" }
   root "tests#index"
 
   # Статическая страница О нас
