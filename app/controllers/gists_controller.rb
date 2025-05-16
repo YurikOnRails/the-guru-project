eb0ac04c6c3551f7255b2788344aecdc3a33bf41
yyyrk <andrei.iurik@gmail.com>
2025-05-16 04:06:17 +0300

    fix: gist error

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -5,7 +5,7 @@ class GistsController < ApplicationController
     @test_passage = TestPassage.find(params[:test_passage_id])
     result = GistQuestionService.new(@test_passage.current_question).call
 
-    flash_options = if result.success?
+    flash_options = if result.successful?
       { notice: t(".success", url: view_context.link_to("Gist", result.url, target: "_blank")).html_safe }
     else
       { alert: t(".failure") }
