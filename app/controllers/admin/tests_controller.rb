a0702271b8042e913c81fc5ac0ba178d4725b204
yyyrk <andrei.iurik@gmail.com>
2025-05-17 21:27:25 +0300

    faet: add timer

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -6,19 +6,21 @@ class Admin::TestsController < Admin::BaseController
     @tests = Test.all
   end
 
-  def show; end
+  def show
+    @questions = @test.questions
+  end
 
   def new
     @test = Test.new
   end
 
   def create
-    @test = current_user.author_tests.new(test_params)
+    @test = current_user.authored_tests.new(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test), notice: t(".success")
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
@@ -26,36 +28,36 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to admin_test_path(@test), notice: t(".success")
+      redirect_to admin_test_path(@test)
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   def update_inline
     if @test.update(test_params)
-      redirect_to admin_tests_path, notice: t(".success")
+      redirect_to admin_tests_path
     else
-      render :index, status: :unprocessable_entity
+      render :index
     end
   end
 
   def destroy
     @test.destroy
-    redirect_to admin_tests_path, notice: t(".success")
+    redirect_to admin_tests_path
   end
 
   private
-
-  def set_tests
-    @tests = Test.all
-  end
 
   def set_test
     @test = Test.find(params[:id])
   end
 
+  def set_tests
+    @tests = Test.all
+  end
+
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :timer)
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,14 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
 
   validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :timer, numericality: { only_integer: true, greater_than: 0, allow_nil: true }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_level, ->(level) { where(level: level) }
+  scope :by_category, ->(category_title) { joins(:category).where(categories: { title: category_title }) }
 
   def self.titles_by_category_name(category_name)
     joins(:category)
