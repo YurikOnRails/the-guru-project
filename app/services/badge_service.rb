2657b3882210098a5f44a519c611070e2cd1abff
yyyrk <andrei.iurik@gmail.com>
2025-05-23 08:25:42 +0300

    fix

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -1,1 +1,23 @@
- 
+class BadgeService
+  def initialize(test_passage)
+    @test_passage = test_passage
+    @user = test_passage.user
+  end
+
+  def call
+    return [] unless @test_passage.successful?
+
+    Badge.find_each.filter_map do |badge|
+      award_badge(badge) if badge.award_condition_met?(@test_passage)
+    end
+  end
+
+  private
+
+  def award_badge(badge)
+    UserBadge.create(
+      user: @user,
+      badge: badge
+    )
+  end
+end 
