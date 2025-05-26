c9760002476469d397c208ac31bf39274fbe597d
yyyrk <andrei.iurik@gmail.com>
2025-05-26 06:07:58 +0300

    fix: html

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -4,14 +4,17 @@ class TestPassagesController < ApplicationController
 
   def show; end
 
-  def result; end
+  def result
+    @earned_badges = @test_passage.earned_badges if @test_passage.completed?
+  end
 
   def update
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
       TestsMailer.completed_test(@test_passage).deliver_now
-      redirect_to result_test_passage_path(@test_passage)
+      @earned_badges = BadgeService.new(@test_passage).call
+      redirect_to result_test_passage_path(@test_passage), notice: success_message
     else
       render :show
     end
@@ -21,5 +24,14 @@ class TestPassagesController < ApplicationController
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def success_message
+    message = 'Тест завершен.'
+    if @earned_badges.present?
+      message += " Вы получили #{@earned_badges.count} " +
+                "#{Russian.p(@earned_badges.count, 'бейдж', 'бейджа', 'бейджей')}!"
+    end
+    message
   end
 end
