7b0da8b63bdbcfbae1729eab5138ab6f7cc034bc
yyyrk <andrei.iurik@gmail.com>
2025-05-30 19:09:36 +0300

    fix: Zeitwerk error

--- app/services/badge_rules/registry.rb
+++ app/services/badge_rules/registry.rb
@@ -3,13 +3,15 @@ require_relative "category_complete_rule"
 require_relative "level_complete_rule"
 
 module BadgeRules
-  RULES = {
-    "first_try" => FirstTryRule,
-    "category_complete" => CategoryCompleteRule,
-    "level_complete" => LevelCompleteRule
-  }.freeze
+  module Registry
+    RULES = {
+      "first_try" => FirstTryRule,
+      "category_complete" => CategoryCompleteRule,
+      "level_complete" => LevelCompleteRule
+    }.freeze
 
-  def self.for(rule_type)
-    RULES[rule_type]
+    def self.for(rule_type)
+      RULES[rule_type]
+    end
   end
 end
