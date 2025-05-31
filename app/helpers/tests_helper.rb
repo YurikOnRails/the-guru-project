9a121c331b86060196ea03344bee191cfb1f4ef7
yyyrk <andrei.iurik@gmail.com>
2025-05-31 14:22:00 +0300

    fix: tests_helper

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -4,4 +4,19 @@ module TestsHelper
   def level_description(level)
     LEVELS[level - 1] || "level #{level}"
   end
+
+  def test_level(test)
+    case test.level
+    when 0
+      t('.easy')
+    when 1
+      t('.elementary')
+    when 2
+      t('.intermediate')
+    when 3
+      t('.advanced')
+    else
+      t('.expert')
+    end
+  end
 end
